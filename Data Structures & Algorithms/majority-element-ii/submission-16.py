4d4df6f61c3e309279d6bf8ccfa93d6ffbcc8989
Lynn <147383008+loofsan@users.noreply.github.com>
2026-06-23 10:23:09 -0700

    Add: majority-element-ii - submission-16

---
+++ Data Structures & Algorithms/majority-element-ii/submission-16.py
@@ -0,0 +1,19 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        
+        nmap = {}
+
+        for num in nums:
+            if len(nmap) <= 2:
+                nmap[num] = nmap.get(num, 0) + 1
+            tempMap = {}
+            for num in nmap:
+                if nmap[num] > 1:
+                    tempMap[num] = nmap[num] - 1
+
+        res = []
+        for num in nmap.keys():
+            if nums.count(num) > len(nums) // 3:
+                res.append(num)
+        
+        return res
