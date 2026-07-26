2ae163c2ef5a4d48a24211b62729dd8c1d166d49
Lynn <147383008+loofsan@users.noreply.github.com>
2026-07-26 04:54:57 -0700

    Add: is-anagram - submission-10

---
+++ Data Structures & Algorithms/is-anagram/submission-10.py
@@ -0,0 +1,42 @@
+"""
+- True if two strings are anagrams of each other
+- False if not
+- Question = "What kind of characters?"
+    - lowercase?
+    - uppercase?
+    - symbols?
+    - Numbers?
+- If the two strings are empty, what do we return?
+    - Are they anagrams or not
+
+- Because we know that the strings only include lowercase letters,
+    - it's limited to 26 characters
+
+- We check if len of t == len of s
+    - if two strings have different lengths,
+    they cannot be anagrams of each other 
+- They have the same len
+    - We can create a charCount array
+    - We can loop through one string
+    - for characters we see in s, we add 1
+    - for characters we see in c, we subtract 1
+- we loop through charCount, if it's not 0, 
+    - return False
+- return True
+"""
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        if len(t) != len(s):
+            return False
+        charCount = [0] * 26
+        for i in range(len(s)):
+            charCount[ord(s[i]) - ord('a')] += 1
+            charCount[ord(t[i]) - ord('a')] -= 1
+        
+        for num in charCount:
+            if num != 0:
+                return False
+        
+        return True
+        
